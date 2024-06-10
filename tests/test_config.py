2eb17c5157298a460fa72d2b31f381df5d8890de
Panoramic <ctpfwr75@duck.com>
2024-06-10 21:50:40 +0300

    Rename to vetting bot

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,8 @@
 import unittest
 from unittest.mock import Mock
 
-from my_project_name.config import Config
-from my_project_name.errors import ConfigError
+from vetting_bot.config import Config
+from vetting_bot.errors import ConfigError
 
 
 class ConfigTestCase(unittest.TestCase):
